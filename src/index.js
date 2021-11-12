4eafd03cde0b8d7f0cc7f257db5fb7b6134ae1d8
RaHughes <rogerallenhughes@hotmail.com>
2021-11-12 14:21:26 -0600

    Add buttons to allow book change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import App from './components/App';
-import TitleBar from './components/TitleBar/TitleBar';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
