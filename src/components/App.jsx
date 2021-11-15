7dbe90a8302f8e2539a9a8f7d0cfcbf9087e3b76
RaHughes <rogerallenhughes@hotmail.com>
2021-11-15 12:02:33 -0600

    Add CreateBook and Footer components to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import TitleBar from './TitleBar/TitleBar';
 import BookViewer from './BookViewer/BookViewer';
+import Footer from './Footer/Footer'
+import CreateBook from './CreateBook/CreateBook';
 import './App.css'
 
 class App extends Component {
@@ -38,11 +40,20 @@ class App extends Component {
         })
     }
 
+    createBook = (newBook) => {
+        this.books.push(newBook)
+        this.setState({
+            bookNumber: this.books.length - 1
+        })
+    }
+
     render() {
         return(
-            <div className='container-fluid'>
+            <div className='container-fluid body'>
                 <TitleBar />
                 <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook}/>
+                <CreateBook createNewBook={this.createBook}/>
+                <Footer />
             </div>
         )
     }
