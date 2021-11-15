0085fc694fbdc9204d4b6e9d7af579ab9dcfd032
RaHughes <rogerallenhughes@hotmail.com>
2021-11-15 12:01:14 -0600

    Add Footer component w/ functionality

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return ( 
+        <div className="footer row row-spacer">
+                <p className="footer-text">© devCodeCamp, INC | <a href="https://devcodecamp.com/wp-content/uploads/2020/03/privacypolicy.pdf">Privacy Policy</a> | <a href="https://devcodecamp.com/wp-content/uploads/2020/03/devCodeCamp-Terms-of-Service.pdf">Terms</a></p> 
+        </div>
+     );
+}
+ 
+export default Footer;
